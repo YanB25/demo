0b9e808671cc0524ca9852072817d96b9adfdd8f
yanbin <yanb25@mail2.sysu.edu.cn>
2019-02-03 16:08:42 +0800

    init

---
+++ add/add.go
@@ -0,0 +1,5 @@
+package add
+
+func Add(lhs int, rhs int) int {
+	return lhs + rhs
+}
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/yanb25/demo/add"
+)
+
+func main() {
+	fmt.Printf("2 + 1 = %d\n", add.Add(2, 1))
+}
